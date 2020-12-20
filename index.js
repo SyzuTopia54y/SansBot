90138c8428c38d88e1c26d25366cf9bddb45540a
SyzuTopia54y <76394087+SyzuTopia54y@users.noreply.github.com>
2020-12-20 20:33:40 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const figlet = require('figlet')
+const options = require('./utils/options')
+const { color, messageLog } = require('./utils')
+const HandleMsg = require('./HandleMsg')
+
+const start = (sans = new Client()) => {
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('SANS BOT', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
+    console.log(color('[DEV]'), color('YourName', 'yellow'))
+    console.log(color('[~>>]'), color('BOT Started!', 'green'))
+
+    // Mempertahankan sesi agar tetap nyala
+    sans.onStateChanged((state) => {
+        console.log(color('[~>>]', 'red'), state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') sans.forceRefocus()
+    })
+
+    // ketika bot diinvite ke dalam group
+    sans.onAddedToGroup(async (chat) => {
+	const groups = await sans.getAllGroups()
+	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
+	if (groups.length > groupLimit) {
+	await sans.sendText(chat.id, `Sorry, the group on this Bot is full\nMax Group is: ${groupLimit}`).then(() => {
+	      sans.leaveGroup(chat.id)
+	      sans.deleteChat(chat.id)
+	  }) 
+	} else {
+	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
+	    if (chat.groupMetadata.participants.length < memberLimit) {
+	    await sans.sendText(chat.id, `Sorry, Bot comes out if the group members do not exceed ${memberLimit} people`).then(() => {
+	      sans.leaveGroup(chat.id)
+	      sans.deleteChat(chat.id)
+	    })
+	    } else {
+        await sans.simulateTyping(chat.id, true).then(async () => {
+          await sans.sendText(chat.id, `Hai minna~, Im sans Bot. To find out the commands on this bot type ${prefix}menu`)
+        })
+	    }
+	}
+    })
+
+    // ketika seseorang masuk/keluar dari group
+    sans.onGlobalParicipantsChanged(async (event) => {
+        const host = await sans.getHostNumber() + '@c.us'
+		const welcome = JSON.parse(fs.readFileSync('./settings/welcome.json'))
+		const isWelcome = welcome.includes(event.chat)
+		let profile = await sans.getProfilePicFromServer(event.who)
+		if (profile == '' || profile == undefined) profile = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQcODjk7AcA4wb_9OLzoeAdpGwmkJqOYxEBA&usqp=CAU'
+        // kondisi ketika seseorang diinvite/join group lewat link
+        if (event.action === 'add' && event.who !== host && isWelcome) {
+			await sans.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await sans.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+        }
+        // kondisi ketika seseorang dikick/keluar dari group
+        if (event.action === 'remove' && event.who !== host) {
+			await sans.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await sans.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
+        }
+    })
+
+    sans.onIncomingCall(async (callData) => {
+        // ketika seseorang menelpon nomor bot akan mengirim pesan
+        await sans.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
+        .then(async () => {
+            // bot akan memblock nomor itu
+            await sans.contactBlock(callData.peerJid)
+        })
+    })
+
+    // ketika seseorang mengirim pesan
+    sans.onMessage(async (message) => {
+        sans.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+            .then((msg) => {
+                if (msg >= 3000) {
+                    console.log('[sans]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    sans.cutMsgCache()
+                }
+            })
+        HandleMsg(sans, message)    
+    
+    })
+	
+    // Message log for analytic
+    sans.onAnyMessage((anal) => { 
+        messageLog(anal.fromMe, anal.type)
+    })
+}
+
+//create session
+create(options(true, start))
+    .then((sans) => start(sans))
+    .catch((err) => new Error(err))
